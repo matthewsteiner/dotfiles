b49a8ebd93fff2e095fe2079cd1998a51a2567fd
Matthew Steiner <matthew@rowofpixels.com>
2013-05-13 14:57:58 -0600

    Test if virtualenvwrapper is available before initializing it

--- shell/config/virtualenvwrapper.sh
+++ shell/config/virtualenvwrapper.sh
@@ -1,4 +1,12 @@
 # initialize virtualenvwrapper
-export WORKON_HOME=$HOME/.virtualenvs
-source /usr/local/share/python/virtualenvwrapper.sh
-export PIP_VIRTUALENV_BASE=$WORKON_HOME
+
+# set default virtualenvwrapper path
+if [ -z "$VIRTUALENVWRAPPER_PATH" ]; then
+  export VIRTUALENVWRAPPER_PATH="/usr/local/share/python/virtualenvwrapper.sh"
+fi
+
+if [ -f $VIRTUALENVWRAPPER_PATH ]; then
+  export WORKON_HOME=$HOME/.virtualenvs
+  source /usr/local/share/python/virtualenvwrapper.sh
+  export PIP_VIRTUALENV_BASE=$WORKON_HOME
+fi
