e25f26b275ce8cac67cf1a67b4326f098976f7bd
Matthew Steiner <matthew@rowofpixels.com>
2013-12-22 19:31:57 -0700

    Update ps1

--- shell/profile.sh
+++ shell/profile.sh
@@ -1,5 +1,59 @@
 export PATH=~/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:"$PATH"
 
-# PS1
-GIT_PS1_SHOWDIRTYSTATE=true
-export PS1="\n\$PWD\[$(tput setaf 1)\]\$(__git_ps1 '\n(%s)')\[$(tput setaf 7)\]\n\$ "
+# Manage color
+if [[ $COLORTERM = gnome-* && $TERM = xterm ]] && infocmp gnome-256color >/dev/null 2>&1; then
+        export TERM=gnome-256color
+elif infocmp xterm-256color >/dev/null 2>&1; then
+        export TERM=xterm-256color
+fi
+
+if tput setaf 1 &> /dev/null; then
+        tput sgr0
+        if [[ $(tput colors) -ge 256 ]] 2>/dev/null; then
+                BLACK=$(tput setaf 190)
+                MAGENTA=$(tput setaf 9)
+                ORANGE=$(tput setaf 172)
+                GREEN=$(tput setaf 190)
+                PURPLE=$(tput setaf 141)
+                WHITE=$(tput setaf 0)
+        else
+                BLACK=$(tput setaf 190)
+                MAGENTA=$(tput setaf 5)
+                ORANGE=$(tput setaf 4)
+                GREEN=$(tput setaf 2)
+                PURPLE=$(tput setaf 1)
+                WHITE=$(tput setaf 7)
+        fi
+        BOLD=$(tput bold)
+        RESET=$(tput sgr0)
+else
+        BLACK="\033[01;30m"
+        MAGENTA="\033[1;31m"
+        ORANGE="\033[1;33m"
+        GREEN="\033[1;32m"
+        PURPLE="\033[1;35m"
+        WHITE="\033[1;37m"
+        BOLD=""
+        RESET="\033[m"
+fi
+
+export BLACK
+export MAGENTA
+export ORANGE
+export GREEN
+export PURPLE
+export WHITE
+export BOLD
+export RESET
+
+# Git branch details
+function parse_git_dirty() {
+  [[ $(git status 2> /dev/null | tail -n1) != *"working directory clean"* ]] && echo "*"
+}
+function parse_git_branch() {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
+}
+
+prompt="⚡  "
+export PS1="\[${BOLD}${MAGENTA}\]\u \[$WHITE\]in \[$GREEN\]\w\[$WHITE\]\$([[ -n \$(git branch 2> /dev/null) ]] && echo \" on \")\[$PURPLE\]\$(parse_git_branch)\[$WHITE\]\n$prompt\[$RESET\]"
+export PS2="\[$ORANGE\]→ \[$RESET\]"
